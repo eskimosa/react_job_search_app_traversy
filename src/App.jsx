5605898c4b0aeea854c5ff5be664a2d85caac113
eskimosa <esskimosa@gmail.com>
2024-05-15 21:13:25 +0200

    Added POST request to add job

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,43 @@
 import React from 'react';
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import HomeCards from './components/HomeCards';
-import JobListings from './components/JobListings';
-import ViewAllJobs from './components/ViewAllJobs';
 import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'; 
 import HomePage from './pages/HomePage';
 import MainLayout from './layouts/MainLayout';
 import JobsPage from './pages/JobsPage';
 import NotFoundPage from './pages/NotFoundPage';
 import JobPage, { jobLoader } from './pages/JobPage';
+import AddJobPage from './pages/AddJobPage';
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<MainLayout />}>
-      <Route index element={<HomePage />} />
-      <Route path="/jobs" element={<JobsPage />} />
-      <Route path="*" element={<NotFoundPage />} />
-      <Route path="/jobs/:id" element={<JobPage />} loader={ jobLoader } />
-    </Route>
-    )
-  );
 
 
 const App = () => {
+  // Add New Job
+  const addJob = async (newJob) => {
+    const res = await fetch('/api/jobs/', {
+      method: 'POST',
+      headers: {
+        'Cntent-Type': 'application/json',
+      },
+      body: JSON.stringify(newJob),
+    });
+    return;
+  };
+
+  const deleteJob = async (id) => {
+    console.log('delete job', id);
+  };
+  
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/jobs" element={<JobsPage />} />
+        <Route path="/jobs/:id" element={<JobPage />} loader={ jobLoader } />
+        <Route path="/add-job" element={<AddJobPage addJobSubmit={addJob}/>} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+      )
+    );
+
   return <RouterProvider router={ router }/>
 };
 
